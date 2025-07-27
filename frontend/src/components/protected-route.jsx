94a9c4482c07604fc2fdcc7840b01f60f61958f1
Winona Murphy <wviolinm@gmail.com>
2025-07-27 02:15:05 -0400

    Create useEffect that only runs on mount and dismount, and checks session status when site first loads, and every time it changes with an event listener in protected-route.jsx, step 35

--- frontend/src/components/protected-route.jsx
+++ frontend/src/components/protected-route.jsx
@@ -6,4 +6,22 @@ export default function ProtectedRoute({ children }){
     const [session, setSession] = useState();
     const [isSessionChecked, setIsSessionChecked] = useState(false);
 
+    useEffect(() => {
+        // Check if there's a session on component mount
+        supabase.auth.getSession().then(({ data: { session }}) => {
+            setSession(() => session ?? null);
+            setIsSessionChecked(() => true);
+        });
+
+        // Set up an event listener for auth state changes
+        const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(() => session ?? null);
+        });
+
+        // Clean up the listener on unmount
+        return () => {
+            authListener?.subscription.unsubscribe();
+        };
+    }, []);
+
 }
