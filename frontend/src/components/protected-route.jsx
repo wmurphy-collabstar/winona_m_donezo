2f88b02c6df625876ca57b7c2214a900ed5c4c78
Winona Murphy <wviolinm@gmail.com>
2025-07-27 02:10:39 -0400

    Add necessary imports for protected-route.jsx file, including React hooks, Navigate, and supabase itself, step 33

--- frontend/src/components/protected-route.jsx
+++ frontend/src/components/protected-route.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import supabase from "../client";
+
 export default function ProtectedRoute({ children }){
 
 }
