49c3f89097f2fa168da3e7607e6ff6aaf340deb1
Winona Murphy <wviolinm@gmail.com>
2025-07-27 02:11:55 -0400

    Set up state to save session and whether session was checked using useState in protected-route.jsx, step 34

--- frontend/src/components/protected-route.jsx
+++ frontend/src/components/protected-route.jsx
@@ -3,5 +3,7 @@ import { Navigate } from "react-router-dom";
 import supabase from "../client";
 
 export default function ProtectedRoute({ children }){
+    const [session, setSession] = useState();
+    const [isSessionChecked, setIsSessionChecked] = useState(false);
 
 }
