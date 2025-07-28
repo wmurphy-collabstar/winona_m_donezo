b4d8f37f5d540f2e8f66926b3a2e5fae2785457a
Winona Murphy <wviolinm@gmail.com>
2025-07-28 00:12:35 -0400

    Create TodoModal component that renders a modal that helps the user create a new todo, with the option to close the modal in todos.jsx, step 46

--- frontend/src/pages/todos.jsx
+++ frontend/src/pages/todos.jsx
@@ -12,6 +12,47 @@ export default function Todos(){
     );
   }
 
+  function TodoModal() {
+    return (
+      <dialog ref={modalRef} className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">New Todo</h3>
+          <form>
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Name of Todo</span>
+              </div>
+              <input 
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Description
+                </span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+            </label>
+            <div className="modal-action">
+              <button type="submit" className="btn btn-primary">
+                Create Todo
+              </button>
+              <button type="button" className="btn btn-ghost">
+                Close
+              </button>
+            </div>
+          </form>
+        </div>
+      </dialog>
+    );
+  }
+
   return (
     <>
       <NewTodoButton/>
