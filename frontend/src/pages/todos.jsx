bf2ae1f82f7c5a78b0b8239b839955f74086a80e
Winona Murphy <wviolinm@gmail.com>
2025-07-28 00:16:49 -0400

    Create a toggleNewTodoModal function that will use the modalRef object to open and close the modal based on its previous state in todos.jsx, steps 48-50

--- frontend/src/pages/todos.jsx
+++ frontend/src/pages/todos.jsx
@@ -6,6 +6,17 @@ export default function Todos(){
 
   const modalRef = useRef();
 
+  const toggleNewTodoModal = () => {
+    // Check if the modal is currently open by accessing the `open` property of `modalRef`.
+    if (modalRef.current.open){
+      // If the modal is open, close it by calling the `close()` method
+      modalRef.current.close();
+    } else {
+      // If the modal is not open, open it by calling the `showModal()` method.
+      modalRef.current.showModal();
+    }
+  }
+
   function NewTodoButton(){
     return (
       <button className="btn btn-primary" onClick={() => toggleNewTodoModal()}>
