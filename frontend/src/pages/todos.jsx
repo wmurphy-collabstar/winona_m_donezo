ddf8747f02ca42db5ca6f6ec320f2df1ae83f390
Winona Murphy <wviolinm@gmail.com>
2025-07-28 00:10:21 -0400

    Create the NewTodoButton component and render it in the Todos component in todos.jsx, steps 43-45

--- frontend/src/pages/todos.jsx
+++ frontend/src/pages/todos.jsx
@@ -3,7 +3,18 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 
 export default function Todos(){
+
+  function NewTodoButton(){
+    return (
+      <button className="btn btn-primary" onClick={() => toggleNewTodoModal()}>
+        New Todo
+      </button>
+    );
+  }
+
   return (
-    <></>
+    <>
+      <NewTodoButton/>
+    </>
   )
 }
